e4a6a189a2797bfbec1817118e4a8e6e5913bb13
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-01 16:46:47 -0300

    update

---
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/model/AppointmentModel.java
@@ -0,0 +1,22 @@
+package com.MedicalAppointment.Appointment.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class AppointmentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long idUser;
+    private String date;
+    private int numberTurn;
+    
+}

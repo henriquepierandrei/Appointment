485263b4114cd10d0d62346b3f18c4c5755140b6
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-01 16:32:29 -0300

    create userModel

---
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/model/UserModel.java
@@ -0,0 +1,30 @@
+package com.MedicalAppointment.Appointment.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Size(max = 15, min = 2, message = "Error with name")
+    private String name;
+
+    @Size(max = 125, min = 2, message = "Error with lastname")
+    private String lastName;
+
+    private String email;
+    private String password;
+    private String cpf;
+    private String yearOld;
+
+}

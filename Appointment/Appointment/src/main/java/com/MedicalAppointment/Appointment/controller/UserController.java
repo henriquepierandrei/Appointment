94956356f7b8996a86e9e3ecf80676c7424d3957
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-02 20:30:15 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/UserController.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/UserController.java
@@ -1,10 +1,22 @@
 package com.MedicalAppointment.Appointment.controller;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @PostMapping("/appointment/create")
+    public ResponseEntity create(){
+
+
+
+
+
+
+       return ResponseEntity.badRequest().build();
+    }
 }

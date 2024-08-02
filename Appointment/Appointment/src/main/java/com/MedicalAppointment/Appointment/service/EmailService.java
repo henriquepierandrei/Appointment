597bd7b395613737bbbef28bc6a334ccf7be7cc5
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-02 13:32:59 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/service/EmailService.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/service/EmailService.java
@@ -27,9 +27,9 @@ public class EmailService {
             // Define o conteúdo da mensagem em HTML
             String htmlContent = "<html>"
                     + "<body>"
-                    + "<h1 style='color: #4CAF50;'>Seu Código de Verificação</h1>"
-                    + "<p>Seu código está logo abaixo:</p>"
-                    + "<p style='font-size: 20px; color: #FF5722;'><b>" + code() + "</b></p>"
+                    + "    <h1 style='color: #ffffff;background-color: rgb(31, 110, 228);width: max-content;padding: 5px;border-radius: 5px;'>Seu Código de Verificação</h1>\n"
+                    + "<p> style: 'font-weight:bold;' Seu código está logo abaixo:</p>"
+                    + "    + \"<p style='font-size: 20px; color: #ffffff;background-image: linear-gradient(to right,rgb(48, 46, 46),rgb(58, 53, 53));padding: 4px;border-radius: 10px 0px;'><b>\" + code() + \"</b></p>\"\n"
                     + "<p>Por favor, não compartilhe este código com ninguém.</p>"
                     + "</body>"
                     + "</html>";

d9d15d91d07a8493db3a7301a46b5922840d36ad
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-02 10:50:57 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
@@ -1,4 +1,11 @@
 package com.MedicalAppointment.Appointment.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 }

40e81fe9588bbc0d3869280eac7cd1d1c2e75a6a
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-03 20:54:28 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
@@ -5,10 +5,7 @@ import com.MedicalAppointment.Appointment.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +17,7 @@ public class AdminController {
     private final AppointmentRepository appointmentRepository;
 
     @GetMapping("/list/appointment/{date}")
-    public ResponseEntity getAppointmentByDate(@RequestParam(value = "date") String date){
+    public ResponseEntity getAppointmentByDate(@PathVariable(value = "date") String date){
         List<AppointmentModel> appointmentModelList = this.appointmentRepository.findByDate(date);
         if (appointmentModelList.isEmpty()){return ResponseEntity.badRequest().build();}
         return ResponseEntity.status(HttpStatus.FOUND).body(appointmentModelList);
---
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/service/GenerateCodeAcessService.java
@@ -0,0 +1,9 @@
+package com.MedicalAppointment.Appointment.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenerateCodeAcessService {
+    
+}

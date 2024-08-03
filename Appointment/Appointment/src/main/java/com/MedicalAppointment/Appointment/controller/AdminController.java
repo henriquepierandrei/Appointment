7609010f1f22403921ffe7830d4a1cb7b057f8d3
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-03 20:44:43 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/controller/AdminController.java
@@ -1,11 +1,29 @@
 package com.MedicalAppointment.Appointment.controller;
 
+import com.MedicalAppointment.Appointment.model.AppointmentModel;
+import com.MedicalAppointment.Appointment.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
+    private final AppointmentRepository appointmentRepository;
+
+    @GetMapping("/list/appointment/{date}")
+    public ResponseEntity getAppointmentByDate(@RequestParam(value = "date") String date){
+        List<AppointmentModel> appointmentModelList = this.appointmentRepository.findByDate(date);
+        if (appointmentModelList.isEmpty()){return ResponseEntity.badRequest().build();}
+        return ResponseEntity.status(HttpStatus.FOUND).body(appointmentModelList);
+    }
+
 }
--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/repository/AppointmentRepository.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/repository/AppointmentRepository.java
@@ -5,14 +5,16 @@ import com.MedicalAppointment.Appointment.model.AppointmentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, Long> {
 
-    boolean findByDate(String date);
-
 
     // Método para encontrar compromissos por data e hora
     Optional<AppointmentModel> findByDateAndTimeAppointmentEnum(String date, String time);
+
+
+    List<AppointmentModel> findByDate(String date);
 }

bc90d62e23cbeff30c07558669d8031d7c49177a
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-04 12:30:22 -0300

    update

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/service/GenerateCodeAcessService.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/service/GenerateCodeAcessService.java
@@ -3,7 +3,18 @@ package com.MedicalAppointment.Appointment.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class GenerateCodeAcessService {
-    
+
+    public String generateCode(){
+        Random random = new Random();
+        StringBuilder numberString = new StringBuilder();
+        for(int i = 0; i < 6; i++){
+            int number = random.nextInt(0,9+1);
+            numberString.append(String.valueOf(number));
+        }
+        return numberString.toString();
+    }
 }

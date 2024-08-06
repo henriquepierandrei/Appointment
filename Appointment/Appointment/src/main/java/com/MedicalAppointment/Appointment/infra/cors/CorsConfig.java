24c2786b91f63aa2c4fa777303de373865437c77
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-06 09:09:04 -0300

    upddate

--- Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/infra/cors/CorsConfig.java
+++ Appointment/Appointment/src/main/java/com/MedicalAppointment/Appointment/infra/cors/CorsConfig.java
@@ -10,7 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500") // URL
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
